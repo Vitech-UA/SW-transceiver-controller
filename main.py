f83d38b40af3a8cbf9ce813efd061e7ca7854349
ViktorStarovit <starovit.v@ajax.systems>
2023-02-19 22:21:27 +0200

    Написав підтягування списку доступних портів.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5.QtCore import QIODevice
 
 app = QtWidgets.QApplication([])
 ui = uic.loadUi("ui.ui")
 ui.setWindowTitle("Serial connector")
-ui.show()
 
+serial = QSerialPort()
+serial.setBaudRate(115200)
+portlist = []
+ports = QSerialPortInfo.availablePorts()
+for port in ports:
+    portlist.append(port.portName())
+ui.ports_combobox.addItems(portlist)
+
+ui.show()
 app.exec()
