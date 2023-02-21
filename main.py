733ddd13f2134ce863316ba7c6fb2eb1f937bbc5
ViktorStarovit <starovit.v@ajax.systems>
2023-02-21 22:39:42 +0200

    Завів керування світлодіодом

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.QtCore import QIODevice
+from uart_connector import UartConnector
+
+uart_port = UartConnector()
+uart_port.bode_rate = 11520
 
 app = QtWidgets.QApplication([])
 ui = uic.loadUi("ui.ui")
@@ -11,8 +15,36 @@ serial.setBaudRate(115200)
 portlist = []
 ports = QSerialPortInfo.availablePorts()
 for port in ports:
-    portlist.append(port.portName())
-ui.ports_combobox.addItems(portlist)
+    portlist.append("{}".format(port.portName()))
+    ui.ports_combobox.addItems(portlist)
 
+
+def open_port():
+    serial.setPortName(ui.ports_combobox.currentText())
+    serial.open(QIODevice.ReadWrite)
+
+
+def close_port():
+    serial.setPortName(ui.ports_combobox.currentText())
+    serial.close()
+
+
+def on_read():
+    RX_RAW = serial.readLine()
+    RX_STR = str(RX_RAW, "utf-8")
+    print(RX_STR.strip())
+
+
+def led_control():
+    if ui.check_box_led.isChecked():
+        serial.write("set_led 1\r\n".encode())
+    if not ui.check_box_led.isChecked():
+        serial.write("set_led 0\r\n".encode())
+
+
+ui.close_btn.clicked.connect(close_port)
+ui.open_btn.clicked.connect(open_port)
+ui.check_box_led.stateChanged.connect(led_control)
+serial.readyRead.connect(on_read)
 ui.show()
 app.exec()
---
+++ uart_connector.py
@@ -0,0 +1,13 @@
+
+
+class UartConnector:
+    def __init__(self):
+        self.bode_rate = 11520
+
+    def send_command(self, command: str):
+        pass
+
+    def parse_answer(self, message: str):
+        answer = []
+        return answer
+
